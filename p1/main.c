9fbbe35b2adaf45d4f8f468454dd39d090e848fc
2025050067 <khk013000@gmail.com>
2026-04-29 15:40:54 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -5,22 +5,21 @@ int main()
     bool none=true;
     int cost, A=900,B=750,C=200,a=0,b=0,c=0;
     scanf("%d",&cost);
-    for(int a=0;a*A<cost;++a){
-        for(int b=0;b*B<cost;++b){
+    for(int a=1;a*A<cost;a++){
+        for(int b=2;b*B<cost;b+=2){
             
-            for(int c=0;c*C<cost;++c){
+            for(int c=1;c*C<cost;c++){
                 if(a!=0&&b!=0&&c!=0){
-                    if(b%2==0){
+                    
                         if(c<a||c<b){
-                            int temp=0;
-                            temp=cost-(a*A+b*B+c*C);
-                            if(temp==0){
+                           
+                            if(cost==a*A+b*B+c*C){
                                 printf("%d %d %d\n",a,b,c);
                                 none=false;
                             }
                         }
                         
-                    }
+                    
                 }
             }
         }
